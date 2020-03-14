56ba336924c3de89426e51317c9f6b2e352aba6c
David Martos <email@davidmartos.es>
2020-03-14 10:27:01 +0100

    User auth error handling

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -3,12 +3,19 @@ var Usuarios = require('../models/usuario')
 var service = require('../services/service');
 
 exports.emailLogin = function(req, res) {
-    Usuarios.findOne({email: req.body.email.toLowerCase()}, function(err, usuario) {
-        // Comprobar si hay errores
-        // Si el usuario existe o no
-        // Y si la contraseña es correcta
-        return res
-            .status(200)
-            .send({token: service.createToken(usuario)});
-    });
+    Usuarios.findOne({
+            email: req.body.email.toLowerCase()
+        }, function (err, usuario) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Se ha producido un error al obtener el usuario'
+                })
+            }
+            if (!usuario) {
+                return res.status(404).json({
+                    message: 'Usuario no existe'
+                })
+            }
+            return res.json(usuario)
+        })
 };
