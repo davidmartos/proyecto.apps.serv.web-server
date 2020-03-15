a2baac8bd2027cbf8edbda384fa2f832df10a445
Victor <victor.rillo@alumnos.ui1.es>
2020-03-15 11:36:13 +0100

    libros y prestamos

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,14 +1,21 @@
+var libroController = require('../controllers/librosController')
+var reservaController = require('../controllers/reservasController')
+
 var indexController = {};
 
 indexController.index = function(req, res){
     res.render('index',{});
 };
 indexController.libros = function(req, res){
-    res.render('libros',{});
+    libroController.list(req, res2 => {
+        res.render('libros', {books: res2});
+    });
 };
 
 indexController.prestamos = function(req, res){
-    res.render('prestamos',{});
+    reservaController.list(req, res2 => {
+        res.render('prestamos', {reservas: res2});
+    });
 };
 
 indexController.login = function(req, res){
